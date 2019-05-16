cfa15f8cfbdb876aaae22f799e3fa6fd0d0f0cfc
AntoniaSaez <asaez1@miuandes.cl>
2019-05-15 23:56:11 -0400

    punto 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  	has_many :user_tickets	
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
+
+  	has_many :user_tickets
 	has_many :tickets, :through => :user_tickets
 
 	validates :email, presence: true, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/}, uniqueness: true
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -261,6 +261,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :facebook, "720282061728777", "00182832c1a942cb1e0558dd200c6754"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
