7ab59d7ab03cf25e32334aa9878e7e44c6475e7b
AntoniaSaez <asaez1@miuandes.cl>
2019-05-16 20:31:01 -0400

    Punto 6 listoco

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,12 @@ class User < ApplicationRecord
 	validates :phone, length: {minimum: 9, maximum: 12}, allow_blank: true
   	validates :password, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "assword must be between 8 to 12 alphanumeric characters"}
 
-
+    def self.from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+    
+      end
+    end
 
 end
